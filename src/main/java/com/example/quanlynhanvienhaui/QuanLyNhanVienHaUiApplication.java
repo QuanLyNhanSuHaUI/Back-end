e58efbe314e58701e87dbda2f6e9e0da2bb9a789
thnhpahm2710 <thnhpahm2710@gmail.com>
2025-09-21 13:11:05 +0700

    create properties

---
+++ src/main/java/com/example/quanlynhanvienhaui/QuanLyNhanVienHaUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.quanlynhanvienhaui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyNhanVienHaUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyNhanVienHaUiApplication.class, args);
+    }
+
+}
