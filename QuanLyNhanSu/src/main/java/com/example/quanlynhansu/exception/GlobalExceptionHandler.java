a75a44b8b3c04241d645e8df1e28ebfc1c5a551c
Phạm Đức Thành <thnhpahm2710@gmail.com>
2025-12-09 11:02:40 +0700

    feat: update employee

--- QuanLyNhanSu/src/main/java/com/example/quanlynhansu/exception/GlobalExceptionHandler.java
+++ QuanLyNhanSu/src/main/java/com/example/quanlynhansu/exception/GlobalExceptionHandler.java
@@ -4,7 +4,6 @@ import com.example.quanlynhansu.base.RestData;
 import com.example.quanlynhansu.base.VsResponseUtil;
 import com.example.quanlynhansu.constant.ErrorMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
