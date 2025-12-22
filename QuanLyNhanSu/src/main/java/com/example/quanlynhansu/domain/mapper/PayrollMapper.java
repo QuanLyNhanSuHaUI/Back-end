fb1e4f9bdbcec94b0b26d5af4d7f9a7c8299e15e
Phạm Đức Thành <thnhpahm2710@gmail.com>
2025-12-22 08:23:31 +0700

    feat: update payroll

--- QuanLyNhanSu/src/main/java/com/example/quanlynhansu/domain/mapper/PayrollMapper.java
+++ QuanLyNhanSu/src/main/java/com/example/quanlynhansu/domain/mapper/PayrollMapper.java
@@ -1,9 +1,13 @@
 package com.example.quanlynhansu.domain.mapper;
 
 import com.example.quanlynhansu.domain.dto.request.payroll.PayrollCreationRequest;
+import com.example.quanlynhansu.domain.dto.request.payroll.PayrollUpdateRequest;
 import com.example.quanlynhansu.domain.dto.response.PayrollResponse;
 import com.example.quanlynhansu.domain.entity.Payroll;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
 
@@ -15,5 +19,8 @@ public interface PayrollMapper {
     PayrollResponse toPayrollResponse(Payroll payroll);
 
     List<PayrollResponse> toPayrollResponsePage(List<Payroll> payrolls);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updatePayrollResponse(PayrollUpdateRequest request, @MappingTarget Payroll payroll);
 
 }
--- QuanLyNhanSu/src/main/java/com/example/quanlynhansu/service/PayrollService.java
+++ QuanLyNhanSu/src/main/java/com/example/quanlynhansu/service/PayrollService.java
@@ -1,7 +1,9 @@
 package com.example.quanlynhansu.service;
 
 import com.example.quanlynhansu.domain.dto.request.payroll.PayrollCreationRequest;
+import com.example.quanlynhansu.domain.dto.request.payroll.PayrollUpdateRequest;
 import com.example.quanlynhansu.domain.dto.response.PayrollResponse;
+import com.example.quanlynhansu.domain.entity.Payroll;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -14,5 +16,7 @@ public interface PayrollService {
     PayrollResponse getPayrollById(String id);
 
     Page<PayrollResponse> getPayrollByConditions(BigDecimal baseSalary, Integer month, Integer year, Pageable pageable);
+
+    PayrollResponse updatePayroll(PayrollUpdateRequest request, String id);
 
 }
